5e998e0d8a840bd6880f39c89938e35878b0a0ed
jeffravi <jeffravi@iu.edu>
2017-11-21 14:03:06 -0500

    Login DB features added

---
+++ database/seeds/RoleTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Role;
+
+class RoleTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $role_admin = new Role();
+        $role_admin->name = ‘admin’;
+        $role_admin->description = "Admin for an applicatinon";
+        $role_admin->save();
+    }
+}
