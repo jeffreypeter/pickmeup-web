b0c8b6092c098e26b52f17edcd8640ee7958965d
Dwayne <dwayned0456@gmail.com>
2017-12-06 13:11:16 -0500

    Driver Screen

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -29,7 +29,7 @@ class UsersTableSeeder extends Seeder
         // 2
         $user = new User();
         $user->name ='Dwayne';
-        $user->email = 'wayne@iu.edu';
+        $user->email = 'dsouza@iu.edu';
         $user->phone = '7122728408';
         $user->password=bcrypt('testing');
         $user->organization='BISM';
@@ -50,6 +50,116 @@ class UsersTableSeeder extends Seeder
         $user = new User();
         $user->name ='elan';
         $user->email = 'elan@iu.edu';
+        $user->phone = '1122728408';
+        $user->password=bcrypt('testing');
+        $user->organization='BISM';
+        $user->address='home';
+        $user->active=true;
+        $user->save();
+		
+		// 5
+        $user = new User();
+        $user->name ='Jason';
+        $user->email = 'jason@iu.edu';
+        $user->phone = '1122728408';
+        $user->password=bcrypt('testing');
+        $user->organization='BISM';
+        $user->address='home';
+        $user->active=true;
+        $user->save();
+		
+		// 6
+        $user = new User();
+        $user->name ='Abbey';
+        $user->email = 'Abbey@iu.edu';
+        $user->phone = '1122728408';
+        $user->password=bcrypt('testing');
+        $user->organization='BISM';
+        $user->address='home';
+        $user->active=true;
+        $user->save();
+		
+		// 7
+        $user = new User();
+        $user->name ='Elye';
+        $user->email = 'elye@iu.edu';
+        $user->phone = '1122728408';
+        $user->password=bcrypt('testing');
+        $user->organization='BISM';
+        $user->address='home';
+        $user->active=true;
+        $user->save();
+		
+		// 8
+        $user = new User();
+        $user->name ='Prajna';
+        $user->email = 'prajna@iu.edu';
+        $user->phone = '1122728408';
+        $user->password=bcrypt('testing');
+        $user->organization='BISM';
+        $user->address='home';
+        $user->active=true;
+        $user->save();
+		
+		// 9
+        $user = new User();
+        $user->name ='Mercury';
+        $user->email = 'mercury@iu.edu';
+        $user->phone = '1122728408';
+        $user->password=bcrypt('testing');
+        $user->organization='BISM';
+        $user->address='home';
+        $user->active=true;
+        $user->save();
+		
+		// 10
+        $user = new User();
+        $user->name ='Vanessa';
+        $user->email = 'vanessa@iu.edu';
+        $user->phone = '1122728408';
+        $user->password=bcrypt('testing');
+        $user->organization='BISM';
+        $user->address='home';
+        $user->active=true;
+        $user->save();
+		
+		// 11
+        $user = new User();
+        $user->name ='Lynn';
+        $user->email = 'lynn@iu.edu';
+        $user->phone = '1122728408';
+        $user->password=bcrypt('testing');
+        $user->organization='BISM';
+        $user->address='home';
+        $user->active=true;
+        $user->save();
+		
+		// 12
+        $user = new User();
+        $user->name ='Clare';
+        $user->email = 'clare@iu.edu';
+        $user->phone = '1122728408';
+        $user->password=bcrypt('testing');
+        $user->organization='BISM';
+        $user->address='home';
+        $user->active=true;
+        $user->save();
+		
+		// 13
+        $user = new User();
+        $user->name ='Ife';
+        $user->email = 'ife@iu.edu';
+        $user->phone = '1122728408';
+        $user->password=bcrypt('testing');
+        $user->organization='BISM';
+        $user->address='home';
+        $user->active=true;
+        $user->save();
+		
+		// 14
+        $user = new User();
+        $user->name ='Ed';
+        $user->email = 'ed@iu.edu';
         $user->phone = '1122728408';
         $user->password=bcrypt('testing');
         $user->organization='BISM';
